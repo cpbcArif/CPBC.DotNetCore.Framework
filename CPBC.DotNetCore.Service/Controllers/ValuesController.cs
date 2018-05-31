eae96065c71746f821eef6af8cd81a28f381e797
Arif Mosharraf <arif.mosharraf@bcpharmacists.org>
2018-05-31 14:13:28 -0700

    Upgraded to .net core Framework 2.1

--- CPBC.DotNetCore.Service/Controllers/ValuesController.cs
+++ CPBC.DotNetCore.Service/Controllers/ValuesController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CPBC.DotNetCore.Common.Facade;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
